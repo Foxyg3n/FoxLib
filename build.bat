03bfea167c7c421f684aef33e223648f1c589e9e
Foxyg3n <piotrek.suchecki@vp.pl>
2023-05-26 16:09:24 +0200

    Added JavaDocs

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 echo %1
 @RD /S /Q "me/foxyg3n/FoxLib/%1/"
-"C:/Program Files/Maven/apache-maven-3.6.3/bin/mvn.cmd" install:install-file -DgroupId=me.foxyg3n -DartifactId=FoxLib -Dversion=%1 -Dfile=../FoxLib/target/FoxLib.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+"C:/Program Files/Maven/apache-maven-3.6.3/bin/mvn.cmd" install:install-file -DgroupId=me.foxyg3n -DartifactId=FoxLib -Dversion=%1 -Dfile=../FoxLib/target/FoxLib-%1.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+"C:/Program Files/Maven/apache-maven-3.6.3/bin/mvn.cmd" install:install-file -DgroupId=me.foxyg3n -DartifactId=FoxLib -Dversion=%1 -Dfile=../FoxLib/target/FoxLib-%1-sources.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
