fda32daedcd1486a71d52668932f361b532e9945
Foxyg3n <piotrek.suchecki@vp.pl>
2023-05-26 13:45:29 +0200

    Updated script and fixed 1.1

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-"C:/Program Files/Maven/apache-maven-3.6.3/bin/mvn.cmd" install:install-file -DgroupId=me.foxyg3n -DartifactId=FoxLib -Dversion=1.0 -Dfile=../FoxLib/target/FoxLib.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+echo %1
+@RD /S /Q "me/foxyg3n/FoxLib/%1/"
+"C:/Program Files/Maven/apache-maven-3.6.3/bin/mvn.cmd" install:install-file -DgroupId=me.foxyg3n -DartifactId=FoxLib -Dversion=%1 -Dfile=../FoxLib/target/FoxLib.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
