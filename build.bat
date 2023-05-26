3e8d58aa091223f7235204476bb7d45a5b7dc7f2
Foxyg3n <piotrek.suchecki@vp.pl>
2023-05-26 13:08:52 +0200

    Fixed PersistentDataUtils

---
+++ build.bat
@@ -0,0 +1,1 @@
+"C:/Program Files/Maven/apache-maven-3.6.3/bin/mvn.cmd" install:install-file -DgroupId=me.foxyg3n -DartifactId=FoxLib -Dversion=1.0 -Dfile=../FoxLib/target/FoxLib.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
